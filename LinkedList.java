2ba61a1e661becea627709b0b7c5320749c54884
Aaditya Kumra <aadityakumra@gmail.com>
2022-07-01 18:16:53 +0000

    created isEmpty() method.

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,19 @@
 public class LinkedList{
-
+  Node head;
+  
   public void createStack(int value){
-    Node head=new Node();
+    head=new Node();
     head.value=value;
     System.out.println("Stack created");
+  }
+
+  public boolean isEmpty(){
+    if(head==null){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
 
--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {
   public static void main(String[] args) {
     LinkedList stack=new LinkedList();
+    System.out.println(stack.isEmpty());
     stack.createStack(1);
+    System.out.println(stack.isEmpty());
   }
 }

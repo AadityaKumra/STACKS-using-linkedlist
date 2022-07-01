2340ae6431b62a63a26838b2203ead54a7a2aa3b
Aaditya Kumra <aadityakumra@gmail.com>
2022-07-01 18:13:13 +0000

    created method to initialize stack using linkedlist.

---
+++ LinkedList.java
@@ -0,0 +1,11 @@
+public class LinkedList{
+
+  public void createStack(int value){
+    Node head=new Node();
+    head.value=value;
+    System.out.println("Stack created");
+  }
+
+
+  
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    LinkedList stack=new LinkedList();
+    stack.createStack(1);
   }
 }
